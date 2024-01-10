cc621400aee0b742ead85acd7491dbb96bf97571
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-01-10 20:10:32 +0000

    datatype

---
+++ .devcontainer/01_basics/03_conversion_operation.js
@@ -0,0 +1,10 @@
+let score = null
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1/ false=> 0
---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,14 @@
+let name = "Afreen"
+let age = 18
+let isLoggedIn = false
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => not assign value yet
+symbol => unique
+Object => 
+*/
