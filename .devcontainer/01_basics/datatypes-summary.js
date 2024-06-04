3fd3802661703687d7646e0eae7c93d83e0fe8e6
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-04 06:17:28 +0000

    datatypesummary

---
+++ .devcontainer/01_basics/datatypes-summary.js
@@ -0,0 +1,31 @@
+// 1.Primitive Datatype - 
+// 7 types: String, Number, Boolean, null, undefined, symbol, BigInt.
+
+const score = 100;
+const scoreValue = 100.3;
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+const id = Symbol('123');
+const anotherId = Symbol('123');
+
+
+console.log(id === anotherId);
+
+// 2.Reference Datatype(Non-Primitive) - 
+// Array, Objects, Functions
+// Non-primitive type always return object
+
+const heroes = ["captain america", "avengers", "superman"]
+
+let myObj = {
+    name:"Afreen",
+    age:22,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+console.log(typeof myFunction);
+console.log(typeof heroes);
+console.log(typeof myObj);
