d862835547790f5aa5f1ee77561c0bd99ddedd10
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-04 09:58:22 +0000

    numberandmaths

---
+++ .devcontainer/01_basics/06_numandmaths.js
@@ -0,0 +1,8 @@
+const score = 500
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+const rate = 1000000;
+console.log(rate.toLocaleString('en-IN'));
