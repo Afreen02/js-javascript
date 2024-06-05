e6dcaee8c42b6fa4a4725f2c4e72e67f573d5a94
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-05 07:20:57 +0000

    datesinjs

---
+++ .devcontainer/01_basics/07_datesinJs.js
@@ -0,0 +1,17 @@
+// DATES
+
+let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toDateString());
+// console.log(typeof myDate);
+
+let mycreatedDate = new Date(2024, 0, 5);
+console.log(mycreatedDate.toDateString());
+
+let myCreatedDate = new Date("06-05-2024")
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+// for date in ms - 
