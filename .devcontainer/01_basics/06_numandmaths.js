fddc22f734778825e0c568fbf89e2268b524e09f
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-05 06:47:11 +0000

    numberandmaths changes

--- .devcontainer/01_basics/06_numandmaths.js
+++ .devcontainer/01_basics/06_numandmaths.js
@@ -6,3 +6,7 @@ console.log(balance.toFixed(1));
 
 const rate = 1000000;
 console.log(rate.toLocaleString('en-IN'));
+
+const min = 10
+const max = 20
+console.log(Math.random(Math.random() *( max - min + 1)) + min);
