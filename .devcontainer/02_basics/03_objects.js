009617cbcd80a810952ad1655ace76014e52e43e
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-15 05:43:35 +0000

    Objects in javascript

--- .devcontainer/02_basics/03_objects.js
+++ .devcontainer/02_basics/03_objects.js
@@ -13,3 +13,14 @@ const users ={
 
 console.log(users["email"]);
 console.log(users[mySymbol]);
+
+// *functions*
+
+users.greetings = function(){
+    console.log("Hello Afreen");
+}
+users.greetingtwo = function(){
+    console.log(`Hello Girl, ${this.name}`)
+}
+console.log(users.greetings());
+console.log(users.greetingtwo())
