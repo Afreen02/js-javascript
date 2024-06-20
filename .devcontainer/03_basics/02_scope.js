28c20901c984501adb5b1ac67a1713652814ebfa
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-20 10:31:00 +0000

    scopes in javascript

---
+++ .devcontainer/03_basics/02_scope.js
@@ -0,0 +1,10 @@
+let a = 200
+if(true){
+    let a = 10
+    const b = 50
+    console.log("INNER:", a);
+}
+for(let i = 0; i < Array.length; i++){
+    const element = Array[i];
+}
+console.log(a);
