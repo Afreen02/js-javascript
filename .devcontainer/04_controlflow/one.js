8266d12efdfd734565c620bef2c18ec56b1cfa60
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-21 09:16:45 +0000

    controlflow

---
+++ .devcontainer/04_controlflow/one.js
@@ -0,0 +1,16 @@
+// const isLoggedin = true
+// const temperature = 43 
+
+// if(temperature === 43){
+//     console.log("less then 50");
+// }
+// else{
+//     console.log("temperature is greater then 50")
+// }
+// console.log("Execute");
+
+// const score = 200
+// if(score > 100){
+//     const power = "fly"
+//     console.log(`User power: ${power}`);
+// }
