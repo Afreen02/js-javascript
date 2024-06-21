11dd1d0641d7c3408c2412850e37fb3a8362d8d0
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-21 05:06:23 +0000

    scope changes

--- .devcontainer/03_basics/02_scope.js
+++ .devcontainer/03_basics/02_scope.js
@@ -8,3 +8,37 @@ for(let i = 0; i < Array.length; i++){
     const element = Array[i];
 }
 console.log(a);
+
+
+function one(){
+    const user = "Afreen"
+    function two(){
+        const website = "Instagram"
+        console.log(user)
+    }
+    two()
+}
+one()
+
+
+
+
+if(true){
+    const username = "Afreen"
+    if(username === "Afreen"){
+        const website = "youtube"
+        console.log(username  +  website);
+    }
+}
+
+// ****IMPORTANT****
+
+function addone(num){
+    return num + 1
+}
+addone(5)
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
