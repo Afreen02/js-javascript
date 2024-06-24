6a4a2e6532b2d0e8c16865e0a21c24b6f19ae48b
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-06-24 05:43:00 +0000

    if-else condition

--- .devcontainer/04_controlflow/one.js
+++ .devcontainer/04_controlflow/one.js
@@ -14,3 +14,30 @@
 //     const power = "fly"
 //     console.log(`User power: ${power}`);
 // }
+
+const balance = 1000;
+if(balance < 500){
+    console.log("less then 500");
+}
+else if(balance < 750){
+    console.log("less then 750");
+}
+else if(balance < 900){
+    console.log("less then 750");
+}
+else{
+    console.log("less then 1200");
+}
+
+const userLoggedin = true
+const debitCard = true
+const loggedinGoogle = false
+const loggedinEmail = true
+
+if(userLoggedin && debitCard && 2==3){
+    console.log("Allow to buy course");
+}
+if(loggedinGoogle || loggedinEmail){
+    console.log("User is logged in");
+}
+// ** this "||" for multiple condition **
