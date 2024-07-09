2fd5a334cc47c37ffcf92f8046616502f85b1bef
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-07-09 07:17:18 +0000

    map and chaining

---
+++ .devcontainer/05_iterations/seveniteration.js
@@ -0,0 +1,10 @@
+const myNymbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNymbers.map( (num) => {
+//     return num + 10
+// })
+
+const newNums = myNymbers.map( (num) => num * 10)
+                .map( (num) => num + 1)
+                .filter( (num) => num >= 30)
+console.log(newNums);
