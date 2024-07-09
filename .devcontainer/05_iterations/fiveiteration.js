33b75e688de4c5ad3a20393b7153fd70a85f98f9
Afreen02 <140620884+Afreen02@users.noreply.github.com>
2024-07-09 06:20:13 +0000

    fiveiteration

--- .devcontainer/05_iterations/fiveiteration.js
+++ .devcontainer/05_iterations/fiveiteration.js
@@ -1,1 +1,40 @@
 const coding = ["js", "react", "python", "ruby", "cpp"]
+
+// coding.forEach(function(val){
+//      console.log(val);
+// })
+
+// coding.forEach(greet = (item) => {
+//     // console.log(item);
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach(printme);
+
+coding.forEach((item, index, arr) => {
+    console.log(item, index, arr);
+})
+
+const mySkills = [
+    {
+        languageName:"javascript",
+        languagefileName:"js"
+    },
+    {
+        languageName:"python",
+        languagefileName:"py"
+    },
+    {
+        languageName:"java",
+        languagefileName:"jv"
+    },
+    {
+        languageName:"javascript",
+        languagefileName:"js"
+    },
+]
+mySkills.forEach((item)=>{
+    console.log(item.languageName);
+})
